774f750ad2b3cb756dab7be31f2677ac9789c121
Seah Chin Ying <scy.6char@gmail.com>
2019-09-01 16:53:13 +0800

    Error handle bad requests

--- src/endpoints/upload.js
+++ src/endpoints/upload.js
@@ -2,6 +2,7 @@ const path = require('path')
 import { PostalDatabase } from '../services/postal'
 import * as _ from 'lodash'
 const AWS = require('aws-sdk')
+const Boom = require('@hapi/boom')
 
 AWS.config.update({
   accessKeyId: process.env.ACCESSKEY,
@@ -31,38 +32,43 @@ export const plugin = {
         }
       },
       handler: async (request, h) => {
-        if (!postalService.loaded) await postalService.init()
-        const [head, ...tail] = request.payload.rows
-        let promisedCoords = tail.reduce(
-          (acc, row) => {
-            if (row.length < 2) return acc // filters blank lines
-            const [originPostal, destinationPostal] = [row[1], row[2]].map(x => transformPostals(x))
-            acc[0].push(postalService.getXY(originPostal))
-            acc[1].push(postalService.getXY(destinationPostal))
-            return acc
-          },
-          [[], []]
-        )
+        try {
+          if (!postalService.loaded) await postalService.init()
+          const [head, ...tail] = request.payload.rows
+          let promisedCoords = tail.reduce(
+            (acc, row) => {
+              if (row.length < 2) return acc // filters blank lines
+              const [originPostal, destinationPostal] = [row[1], row[2]].map(x => transformPostals(x))
+              acc[0].push(postalService.getXY(originPostal))
+              acc[1].push(postalService.getXY(destinationPostal))
+              return acc
+            },
+            [[], []]
+          )
 
-        // can technically Promise.all([origins, destinations]) but I don't feel like figuring out the map / zip syntax
-        let origins = await Promise.all(
-          promisedCoords[0].map(p => p.catch(() => null))
-        )
+          // can technically Promise.all([origins, destinations]) but I don't feel like figuring out the map / zip syntax
+          let origins = await Promise.all(
+            promisedCoords[0].map(p => p.catch(() => null))
+          )
 
-        let destinations = await Promise.all(
-          promisedCoords[1].map(p => p.catch(() => null))
-        )
-        const entriesWithPostals = _.zip(origins, destinations, tail)
-          .map(r => {
-            const row = _.flatten(r)
-            if (!row || row.length < 4) return
-            let [origin, destination] = [row[0], row[1]]
-            if (!origin || !destination) return
-            return [row[2], origin.x, origin.y, destination.x, destination.y, ...row.slice(3)]
-          })
-          .filter(x => x !== undefined)
+          let destinations = await Promise.all(
+            promisedCoords[1].map(p => p.catch(() => null))
+          )
+          const entriesWithPostals = _.zip(origins, destinations, tail)
+            .map(r => {
+              const row = _.flatten(r)
+              if (!row || row.length < 4) return
+              let [origin, destination] = [row[0], row[1]]
+              if (!origin || !destination) return
+              return [row[2], origin.x, origin.y, destination.x, destination.y, ...row.slice(3)]
+            })
+            .filter(x => x !== undefined)
 
-        return h.response(entriesWithPostals)
+          return h.response(entriesWithPostals)
+        } catch (err) {
+          console.error(err)
+          Boom.badImplementation('server died')
+        }
       }
     })
   }
